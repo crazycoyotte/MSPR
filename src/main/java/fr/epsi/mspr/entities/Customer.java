690026037821ad487efc343e8922ceaf6f86a0fc
Ptitlu42 <lucas.beyer@gmx.fr>
2023-02-28 15:49:49 +0100

    ajout id et name

--- src/main/java/fr/epsi/mspr/entities/Customer.java
+++ src/main/java/fr/epsi/mspr/entities/Customer.java
@@ -1,5 +1,15 @@
 package fr.epsi.mspr.entities;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+
 public class Customer {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id_customer;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String name_customer;
     
 }
