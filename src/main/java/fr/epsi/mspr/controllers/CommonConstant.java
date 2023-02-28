965baeefab5700416c6cf7650c7fe7e0f420c039
Ptitlu42 <lucas.beyer@gmx.fr>
2023-02-28 15:34:52 +0100

    rename constant

--- src/main/java/fr/epsi/mspr/controllers/CommonConstant.java
+++ src/main/java/fr/epsi/mspr/controllers/CommonConstant.java
@@ -3,10 +3,10 @@ package fr.epsi.mspr.controllers;
 public class CommonConstant {
 
     public static final String ROUTE_ALL = "/";
-    public static final String ROUTE_SHOW_COMMAND = "/{id_command}/show";
+    public static final String ROUTE_SHOW_ORDER = "/{id_order}/show";
     public static final String ROUTE_CREATE = "/create";
-    public static final String ROUTE_EDIT_COMMAND = "/{id_command}/edit";
-    
+    public static final String ROUTE_EDIT_ORDER = "/{id_order}/edit";
+
 
     
 }
