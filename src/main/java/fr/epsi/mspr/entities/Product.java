30541c2ca12f41fafeecfeb791ed6cb9369aaf99
Ptitlu42 <lucas.beyer@gmx.fr>
2023-02-28 13:57:02 +0100

    ajout id et name de product

--- src/main/java/fr/epsi/mspr/entities/Product.java
+++ src/main/java/fr/epsi/mspr/entities/Product.java
@@ -1,5 +1,21 @@
 package fr.epsi.mspr.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
     
+    @Column(unique = true, nullable = false, length = 50)
+    private String name;
+
 }
