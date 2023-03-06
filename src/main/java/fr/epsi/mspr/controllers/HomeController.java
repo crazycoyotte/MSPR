951666d77f240b11c2f5c48049933ea69254041f
Ptitlu42 <lucas.beyer@gmx.fr>
2023-03-06 14:36:29 +0100

    add home controller

---
+++ src/main/java/fr/epsi/mspr/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package fr.epsi.mspr.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "<h1>WESH!</h1>";
+    }
+
+}
--- src/main/java/fr/epsi/mspr/entities/Employee.java
+++ src/main/java/fr/epsi/mspr/entities/Employee.java
@@ -2,9 +2,9 @@ package fr.epsi.mspr.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
 
 import lombok.Data;
 
@@ -12,19 +12,19 @@ import lombok.Data;
 @Data
 public class Employee {
 
-    //@Id
-    //@GeneratedValue(strategy = GenerationType.AUTO)
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.AUTO)
     public long idEmployee;
 
     @Column(nullable = false, length = 255)
     public String lastName;
-    
+
     @Column(nullable = false, length = 255)
     public String fistName;
-    
-	@Column(nullable = false, length = 255)
-	public String login;
-    
+
+    @Column(nullable = false, length = 255)
+    public String login;
+
     @Column(nullable = false, length = 255)
     public String password;
 }
