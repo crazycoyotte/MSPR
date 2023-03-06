291d491bf3a48d9ebb9c1d5f068232662e873184
Florian Sibois <siboisflorian@yahoo.fr>
2023-03-06 15:31:36 +0100

    ajout des arraylist de base

--- src/main/java/fr/epsi/mspr/MsprApplication.java
+++ src/main/java/fr/epsi/mspr/MsprApplication.java
@@ -15,34 +15,7 @@ public class MsprApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsprApplication.class, args);
 		System.out.println("LE PUSH ??");
-		Employee empl = new Employee();
-		empl.idEmployee = 1;
-		empl.login = "JB.Test";
-		empl.fistName = "Jean Bobby";
-		empl.lastName = "Test";
-		empl.password = "1234";
-		Customer Trololo = new Customer();
-		Trololo.idCustomer = 1;
-		Trololo.nameCustomer = "Trololo";
-		Customer Bliblablo = new Customer();
-		Bliblablo.idCustomer = 2;
-		Bliblablo.nameCustomer = "Bliblablo";
-		Product Book = new Product();
-		Book.idProduct = 1;
-		Book.nameProduct = "Le saigneur des agneaux";
-		Product Book2 = new Product();
-		Book2.idProduct = 2;
-		Book2.nameProduct = "Les deux fours";
-		Product Book3 = new Product();
-		Book3.idProduct = 3;
-		Book3.nameProduct = "Le retour du foie";
-		ArrayList<Customer> customerList = new ArrayList<Customer>();
-		customerList.add(Bliblablo);
-		customerList.add(Trololo);
-		ArrayList<Product> productList = new ArrayList<Product>();
-		productList.add(Book);
-		productList.add(Book2);
-		productList.add(Book3);
+
 	}
 
 }
---
+++ src/main/java/fr/epsi/mspr/baseList/package-info.java
@@ -0,0 +1,1 @@
+package fr.epsi.mspr.baseList;
