e2c77f70b65327b33cfe94676c717efddcb8d384
Ptitlu42 <lucas.beyer@gmx.fr>
2023-03-06 16:35:13 +0100

    add login view

--- src/main/java/fr/epsi/mspr/controllers/CommonConstant.java
+++ src/main/java/fr/epsi/mspr/controllers/CommonConstant.java
@@ -6,7 +6,5 @@ public class CommonConstant {
     public static final String ROUTE_SHOW_ORDER = "/{idOrder}/show";
     public static final String ROUTE_CREATE = "/create";
     public static final String ROUTE_EDIT_ORDER = "/{idOrder}/edit";
-
-
-    
+    public static final String ROUTE_LIST_PRODUCTS = "/listProducts";
 }
